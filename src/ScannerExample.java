25ae84980a966c3b2a68075789035197c2b992c0
wkdeogus <yung061830@gmail.com>
2023-05-12 15:11:58 +0900

    test

---
+++ src/ScannerExample.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class ScannerExample {
+    public static void main(String [] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("X 값 입력: ");
+        String strX = scanner.nextLine();
+        int x = Integer.parseInt(strX);
+
+        System.out.print("y 값 입력: ");
+        String stry = scanner.nextLine();
+        int y =  Integer.parseInt(stry);
+
+        int result = x + y;
+        System.out.println("x + y :" + result);
+        System.out.println();
+
+        while (true) {
+            System.out.print("입력 문자열 :");
+            String data = scanner.nextLine();
+            if (data.equals("q")) {
+                break;
+            }
+            System.out.println("출력 문자열 :" + data);
+            System.out.println();
+        }
+
+        System.out.println("종료");
+    }
+}
