a0c15d351831bec8833ee2b68578cfdfc9e2b3b4
wkdeogus <yung061830@gmail.com>
2023-05-15 21:07:38 +0900

    exam1

--- src/test.java
+++ src/test.java
@@ -1,9 +1,9 @@
 public class test {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int a = 20;
         int b = 40;
         int c = 60;
-        float d = (a+b+c)/3;
+        float d = (a + b + c) / 3;
         System.out.print("세 수의 평균은 " + d + " 입니다.");
 
     }
