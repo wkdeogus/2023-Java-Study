b12a2a94726501d52bec9311ce1ea747a2e830c4
wkdeogus <yung061830@gmail.com>
2023-05-29 02:05:59 +0900

    문제1

---
+++ src/Threeweek/Car.java
@@ -0,0 +1,17 @@
+package Threeweek;
+
+public abstract class Car {
+
+    public void startCar() {
+        System.out.println("시동을 겁니다.");
+    }
+
+    public void drive() {
+        System.out.println("일반모드로 주행합니다");
+    }
+
+    public void stopCar() {
+        System.out.println("시동을 끕니다.");
+    }
+
+}
