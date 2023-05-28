ac3c6a5ef6201cc9422c0e3fe14b7e8352a8d991
wkdeogus <yung061830@gmail.com>
2023-05-29 02:21:04 +0900

    문제3

---
+++ src/Threeweek/TurboModeCarExample.java
@@ -0,0 +1,15 @@
+package Threeweek;
+
+public class TurboModeCarExample {
+    public static void main(String[] args) {
+       /*
+        시동을 겁니다.
+        일반모드로 주행합니다
+        터보모드로 주행합니다.
+        일반모드로 주행합니다
+        시동을 끕니다.
+
+        위와 같은 출력값이 나오도록 코드 입력
+        */
+    }
+}
