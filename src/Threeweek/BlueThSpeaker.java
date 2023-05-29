b917b7b10ea07159b8bb9ea12439ef0f2d7ed390
wkdeogus <yung061830@gmail.com>
2023-05-29 22:04:58 +0900

    태영2

---
+++ src/Threeweek/BlueThSpeaker.java
@@ -0,0 +1,10 @@
+package Threeweek;
+
+public class BlueThSpeaker extends Speaker{
+    public BlueThSpeaker(String model, String color) {
+        super();
+        this.model = model;
+        this.color = color;
+        System.out.println("BlueThspeaker 모델 컬러 생성자 실행 ");
+    }
+}
