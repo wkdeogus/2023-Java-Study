231a92ed3b60b00789e691fb56418b55144385e3
wkdeogus <yung061830@gmail.com>
2023-05-31 02:01:32 +0900

    준수1

---
+++ src/Threeweek/imStar.java
@@ -0,0 +1,12 @@
+/*다음은 import를 이용해 문자를 출력하는 프로그램입니다.
+ *  이 프로그램을 상속을 이용해 같은 결과가 나오도록 하는 프로그램으로 바꿔주세요.*/
+
+package Threeweek;
+
+import Threeweek.ImportStar;
+
+public class imStar {
+    public static void main(String[] args) {
+        ImportStar imstar = new ImportStar("*");
+    }
+}
