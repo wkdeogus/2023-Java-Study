aaf0f84add7e83afafed0d214de6675270317602
wkdeogus <yung061830@gmail.com>
2023-06-20 01:47:19 +0900

    태영문제

---
+++ src/weekFive/Dogeat.java
@@ -0,0 +1,14 @@
+package weekFive;
+
+public class Dogeat {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+
+        Apple apple = new Apple();
+        dog.eat(apple);
+
+        Banana banana = new Banana();
+        dog.eat(banana);
+
+    }
+}
